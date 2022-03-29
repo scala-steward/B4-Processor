8af92bb063482675f850ac0f68df84a5e5cc3415
michiya-kato <s194264q@st.go.tuat.ac.jp>
2022-03-29 20:38:36 +0900

    Option_stag.scalaについては完成

--- src/main/scala/Decoder/Option_stag.scala
+++ src/main/scala/Decoder/Option_stag.scala
@@ -9,7 +9,7 @@ import firrtl.Utils.True
 class Option_stag extends Module {
   val io = IO(new Bundle{
     val before_dtag = DecoupledIO(UInt(TAG_WIDTH.W))
-    val reorder_dtag = DecoupledIO(UInt(TAG_WIDTH.W))
+    val reorder_buffer_dtag = DecoupledIO(UInt(TAG_WIDTH.W))
     val r_value = Output(Bool()) // value選択用回路に用いるRビット
     val stag = Output(UInt(8.W)) // 選択したstagを格納
   })
@@ -20,14 +20,14 @@ class Option_stag extends Module {
     */
 
   io.r_value := Wire(MuxCase(1.U, Seq(
-    (io.before_dtag.valid === 0.U && io.reorder_dtag.valid === 0.U) -> 0.U)))
+    (io.before_dtag.valid === 0.U && io.reorder_buffer_dtag.valid === 0.U) -> 0.U)))
 
   /**
    * R=1 ならば，MuxCaseでstagを選択
    */
 
   when(io.r_value) { // Rbit = 1
-    io.stag := RegNext(Mux(io.reorder_dtag.valid, io.reorder_dtag.bits, io.before_dtag.bits))
+    io.stag := RegNext(Mux(io.reorder_buffer_dtag.valid, io.reorder_buffer_dtag.bits, io.before_dtag.bits))
     // 最大発行命令数(i)を増やすならばMux -> MuxCase に変更
   }.otherwise { // Rbit = 0
     io.stag := WireDefault(0.U) // stagが存在しなければstag = 0に設定
