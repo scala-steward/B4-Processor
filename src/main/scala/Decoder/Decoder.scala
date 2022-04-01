fd3748f90642e0c956b64df868df12a5d9486d98
Shogo Takata <s196643z@st.go.tuat.ac.jp>
2022-04-01 17:39:02 +0900

    デコーダに追記

--- src/main/scala/Decoder/Decoder.scala
+++ src/main/scala/Decoder/Decoder.scala
@@ -11,33 +11,67 @@ import consts.Constants.TAG_WIDTH
  */
 class Decoder(instruction_offset: Int) extends Module {
   val io = IO(new Bundle {
-    val imem = Flipped(new IMem2DecoderConnection())
+    val imem = Flipped(DecoupledIO(new IMem2DecoderConnection()))
     val reorder_buffer = new Decoder2ReorderBuffer()
+    val reservationStation = DecoupledIO(new Decoder2ReservationStationEntry)
+    val previous_decoders = Input(Vec(instruction_offset, new Decoder2NextDecoder))
+    val next_decoders = Output(Vec(instruction_offset + 1, new Decoder2NextDecoder))
   })
 
-  val rd = io.imem.instruction(11, 7)
-  val rs1 = io.imem.instruction(19, 15)
-  val rs2 = io.imem.instruction(24, 20)
-  val funct3 = io.imem.instruction(14, 12)
-  val funct7 = io.imem.instruction(31, 25)
-  val imm_I = io.imem.instruction(31, 20)
+  val inst_rd = io.imem.bits.instruction(11, 7)
+  val inst_rs1 = io.imem.bits.instruction(19, 15)
+  val inst_rs2 = io.imem.bits.instruction(24, 20)
+  val inst_funct3 = io.imem.bits.instruction(14, 12)
+  val inst_funct7 = io.imem.bits.instruction(31, 25)
+  val inst_op = io.imem.bits.instruction(6,0)
+  val inst_imm_I = io.imem.bits.instruction(31, 20)
 
   // to reorder buffer
-  io.reorder_buffer.program_counter := io.imem.program_counter
-  io.reorder_buffer.source1.register_source := rs1
-  io.reorder_buffer.source2.register_source := rs2
-  io.reorder_buffer.destination.register_destination := rd
+  io.reorder_buffer.program_counter := io.imem.bits.program_counter
+  io.reorder_buffer.source1.register_source := inst_rs1
+  io.reorder_buffer.source2.register_source := inst_rs2
+  io.reorder_buffer.destination.register_destination.bits := inst_rd
+  // TODO: change to depend on instruction type
+  io.reorder_buffer.destination.register_destination.valid := true.B
+
+  // get matching tags
+  val option_stag = Module(new Option_stag)
+  option_stag.io.reorder_buffer_dtag <> io.reorder_buffer.source1.matching_tag
+
+  // previous decoder to next decoder
+  for (i <- 0 until instruction_offset) {
+    io.next_decoders(i) <> io.previous_decoders(i)
+  }
+  // TODO: apply same logic to valid as reorder buffer destination register
+  io.next_decoders(instruction_offset).destination_tag := 1.U
+  io.next_decoders(instruction_offset).destination_register := 1.U
+  io.next_decoders(instruction_offset).valid := false.B
 
   // TODO: change
   io.reorder_buffer.source1.matching_tag.ready := true.B
   io.reorder_buffer.source1.value.ready := true.B
   io.reorder_buffer.source2.matching_tag.ready := true.B
   io.reorder_buffer.source2.value.ready := true.B
+
+  io.imem.ready <> io.reservationStation.ready
+  io.imem.valid <> io.reservationStation.valid
+
+  // TODO: fill all reservation station fields
+  val rs = io.reservationStation.bits
+  rs.op_code := inst_op
+  rs.function3 := inst_funct3
+  rs.immediate_or_function7 := 0.U
+  rs.source_tag1 := 0.U
+  rs.source_tag2 := 0.U
+  rs.ready1 := false.B
+  rs.ready2 := false.B
+  rs.value1 := 0.U
+  rs.value2 := 0.U
 }
 
 class IMem2DecoderConnection extends Bundle {
-  val instruction = Output(UInt(64.W))
-  val program_counter = Output(UInt(64.W))
+  val instruction = UInt(64.W)
+  val program_counter = UInt(64.W)
 }
 
 class Decoder2ReorderBuffer extends Bundle {
@@ -48,7 +82,7 @@ class Decoder2ReorderBuffer extends Bundle {
   }
 
   class RegisterDestination extends Bundle {
-    val register_destination = Output(UInt(5.W))
+    val register_destination = DecoupledIO(UInt(5.W))
     val destination_tag = Input(UInt(TAG_WIDTH.W))
   }
 
@@ -56,4 +90,22 @@ class Decoder2ReorderBuffer extends Bundle {
   val source2 = new RegisterSource()
   val destination = new RegisterDestination()
   val program_counter = Output(UInt(64.W))
+}
+
+class Decoder2NextDecoder extends Bundle {
+  val valid = Bool()
+  val destination_tag = UInt(5.W)
+  val destination_register = UInt(TAG_WIDTH.W)
+}
+
+class Decoder2ReservationStationEntry extends Bundle {
+  val op_code = UInt(7.W)
+  val function3 = UInt(3.W)
+  val immediate_or_function7 = UInt(12.W)
+  val source_tag1 = UInt(TAG_WIDTH.W)
+  val ready1 = Bool()
+  val value1 = UInt(64.W)
+  val source_tag2 = UInt(TAG_WIDTH.W)
+  val ready2 = Bool()
+  val value2 = UInt(64.W)
 }
--- src/test/scala/Decoder/DecoderTest.scala
+++ src/test/scala/Decoder/DecoderTest.scala
@@ -5,30 +5,89 @@ import chisel3._
 import chisel3.util._
 import chiseltest._
 
+
+
+/**
+ * デコーダをテストしやすくするためにラップしたもの
+ *
+ * @param instruction_offset 同時に扱う命令のうちいくつ目の命令を担当するか
+ */
+class DecoderWrapper(instruction_offset: Int) extends Decoder(instruction_offset) {
+  def initialize(instruction: UInt): Unit = {
+    this.io.imem.bits.program_counter.poke(0.U)
+    this.io.imem.bits.instruction.poke(instruction)
+    this.io.imem.valid.poke(true.B)
+    this.io.reorder_buffer.destination.destination_tag.poke(0.U)
+    this.io.reorder_buffer.source1.matching_tag.valid.poke(true.B)
+    this.io.reorder_buffer.source1.matching_tag.bits.poke(0.U)
+    this.io.reorder_buffer.source1.value.valid.poke(false.B)
+    this.io.reorder_buffer.source1.value.bits.poke(0.B)
+    this.io.reorder_buffer.source2.matching_tag.valid.poke(true.B)
+    this.io.reorder_buffer.source2.matching_tag.bits.poke(0.U)
+    this.io.reorder_buffer.source2.value.valid.poke(false.B)
+    this.io.reorder_buffer.source2.value.bits.poke(0.B)
+  }
+
+  def expect_reorder_buffer(rd: Option[Int], rs1: Int, rs2: Option[Int]): Unit = {
+    // check rd
+    rd match {
+      case None => {
+        this.io.reorder_buffer.destination.register_destination.valid.expect(false.B, "rd should not be valid")
+      }
+      case Some(value) => {
+        this.io.reorder_buffer.destination.register_destination.valid.expect(true.B, "rd should be valid")
+        this.io.reorder_buffer.destination.register_destination.bits.expect(value.U, "rd value doesn't match")
+      }
+    }
+
+    // check rs1
+    this.io.reorder_buffer.source1.register_source.expect(rs1.U, "rs1 doesn't match")
+
+    // check rs2
+    if (rs2.isDefined) {
+      this.io.reorder_buffer.source2.register_source.expect(rs2.get.U, "rs2 doesn't match")
+    }
+  }
+
+  def expect_reservation_station(): Unit ={
+
+  }
+}
+
+/**
+ * デコーダのテスト
+ */
 class DecoderTest extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Decoder"
 
-  it should "pass this dummy test" in {
-    assert(1 + 1 === 2)
+  it should "pass rs1 rs2 rd to reorder buffer" in {
+    test(new DecoderWrapper(0)) { c =>
+      c.initialize("b0000000_00011_00010_000_00001_0000000".U)
+      c.expect_reorder_buffer(Some(1), 2, Some(3))
+    }
   }
 
-  it should "pass rs1 rs2 rd to reorder buffer" in {
-    test(new Decoder(0)) { c =>
-      c.io.imem.program_counter.poke(0.U)
-      c.io.imem.instruction.poke("b0000000_00011_00010_000_00001_0000000".U)
-      c.io.reorder_buffer.destination.destination_tag.poke(0.U)
-      c.io.reorder_buffer.source1.matching_tag.valid.poke(true.B)
-      c.io.reorder_buffer.source1.matching_tag.bits.poke(0.U)
-      c.io.reorder_buffer.source1.value.valid.poke(false.B)
-      c.io.reorder_buffer.source1.value.bits.poke(0.B)
-      c.io.reorder_buffer.source2.matching_tag.valid.poke(true.B)
-      c.io.reorder_buffer.source2.matching_tag.bits.poke(0.U)
-      c.io.reorder_buffer.source2.value.valid.poke(false.B)
-      c.io.reorder_buffer.source2.value.bits.poke(0.B)
+  it should "understand R type instruction" in {
+    test(new DecoderWrapper(0)) { c =>
+      // add x1,x2,x3
+      c.initialize(0x003100b3.U)
+      c.expect_reorder_buffer(Some(1), 2, Some(3))
+    }
+  }
 
-      c.io.reorder_buffer.destination.register_destination.expect(1.U)
-      c.io.reorder_buffer.source1.register_source.expect(2.U)
-      c.io.reorder_buffer.source2.register_source.expect(3.U)
+  it should "understand sd" in {
+    test(new DecoderWrapper(0)) { c =>
+      // sd x1,10(x2)
+      c.initialize(0x00113523.U)
+      c.expect_reorder_buffer(None, 1, Some(2))
+    }
+  }
+
+  it should "understand immediate" in {
+    test(new DecoderWrapper(0)) { c =>
+      // addi x1,x2,20
+      c.initialize(0x01410093.U)
+      c.expect_reorder_buffer(Some(1), 2, None)
     }
   }
 }
