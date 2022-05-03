81525730aa8f7f47d6637b24eb767c773a3c8cbb
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-05-03 14:37:32 +0900

    バイパスのBundle化

--- src/main/scala/b4processor/connections/ExecutionRegisterBypass.scala
+++ src/main/scala/b4processor/connections/ExecutionRegisterBypass.scala
@@ -2,14 +2,14 @@ package b4processor.connections
 
 import b4processor.Parameters
 import chisel3._
+import chisel3.util._
 
 /**
  * ALUからデコーダへバイパスされたデータを送る
- *
  * @param params パラメータ
  */
-class ExecutionRegisterBypass(implicit val params: Parameters) extends Bundle {
-  val destinationTag = Output(UInt(params.tagWidth.W))
-  val value = Output(UInt(64.W))
-  val valid = Output(Bool())
+class ExecutionRegisterBypass(implicit val params:Parameters) extends Bundle {
+  val destinationTag = UInt(params.tagWidth.W)
+  val value = UInt(64.W)
+  val valid = Bool()
 }
