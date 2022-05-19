a4091da5dc3fdef3286a8678ebf7213c7e0132db
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-05-19 13:32:04 +0900

    実行ユニット（暫定）

--- src/main/scala/b4processor/modules/executor/Executor.scala
+++ src/main/scala/b4processor/modules/executor/Executor.scala
@@ -28,11 +28,11 @@ class Executor(implicit params: Parameters) extends Module {
 
   val destinationRegister = Wire(UInt(64.W))
   val immediateOrFunction7Extended = Mux(io.reservationstation.bits.immediateOrFunction7(11), (!0.U(64.W)) & io.reservationstation.bits.immediateOrFunction7, 0.U(64.W) | io.reservationstation.bits.immediateOrFunction7)
-  val newProgramCounter = io.reservationstation.bits.programCounter + immediateOrFunction7Extended.asSInt
+  val branchedProgramCounter = io.reservationstation.bits.programCounter + (immediateOrFunction7Extended ## 0.U(1.W)).asSInt
 
   // set destinationRegister
   io.reservationstation.ready := true.B
-  when(io.reservationstation.valid === true.B) {
+  when(io.reservationstation.valid) {
     destinationRegister := MuxCase(0.U, Seq(
       // 加算
       (instructionChecker.output.instruction === Instructions.Load || (instructionChecker.output.arithmetic === ArithmeticOperations.Addition))
@@ -55,10 +55,10 @@ class Executor(implicit params: Parameters) extends Module {
       // 右シフト(論理)
       (instructionChecker.output.arithmetic === ArithmeticOperations.ShiftRightLogical)
         -> (io.reservationstation.bits.value1 >> io.reservationstation.bits.value2(5, 0)),
+      // Cat(0.U(((io.reservationstation.bits.value2(5, 0)).W)), io.reservationstation.bits.value1(31, (31-io.reservationstation.bits.value2(5, 0)))
       //右シフト(算術)
       (instructionChecker.output.arithmetic === ArithmeticOperations.ShiftRightArithmetic)
         -> (io.reservationstation.bits.value1.asSInt >> io.reservationstation.bits.value2(5, 0)).asUInt,
-      // Cat(0.U((io.reservationstation.bits.value2(5, 0).W)), io.reservationstation.bits.value1(31, (31-io.reservationstation.bits.value2(5, 0)))
       // 比較(格納先：rd)(符号付き)
       (instructionChecker.output.arithmetic === ArithmeticOperations.SetLessThan)
         -> (io.reservationstation.bits.value1.asSInt < io.reservationstation.bits.value2.asSInt).asUInt,
@@ -73,44 +73,67 @@ class Executor(implicit params: Parameters) extends Module {
         -> io.reservationstation.bits.value2,
       // auipc
       (instructionChecker.output.instruction === Instructions.auipc)
-        -> (io.reservationstation.bits.value2 + io.reservationstation.bits.programCounter.asUInt)
-    ))
-
-    io.fetch.programCounter := MuxCase(io.reservationstation.bits.programCounter, Seq(
-      // 分岐
+        -> (io.reservationstation.bits.value2 + io.reservationstation.bits.programCounter.asUInt),
+      // 分岐((
       // Equal
       (instructionChecker.output.branch === BranchOperations.Equal)
-        -> Mux(io.reservationstation.bits.value1 === io.reservationstation.bits.value2,
-        newProgramCounter, 0.S),
+        -> (io.reservationstation.bits.value1 === io.reservationstation.bits.value2),
       // NotEqual
       (instructionChecker.output.branch === BranchOperations.NotEqual)
-        -> Mux(io.reservationstation.bits.value1 =/= io.reservationstation.bits.value2,
-        newProgramCounter, 0.S),
+        -> (io.reservationstation.bits.value1 =/= io.reservationstation.bits.value2),
       // Less Than (signed)
       (instructionChecker.output.branch === BranchOperations.LessThan)
-        -> Mux(io.reservationstation.bits.value1.asSInt < io.reservationstation.bits.value2.asSInt,
-        newProgramCounter, 0.S),
+        -> (io.reservationstation.bits.value1.asSInt < io.reservationstation.bits.value2.asSInt),
       // Less Than (unsigned)
       (instructionChecker.output.branch === BranchOperations.LessThanUnsigned)
-        -> Mux(io.reservationstation.bits.value1 < io.reservationstation.bits.value2,
-        newProgramCounter, 0.S),
+        -> (io.reservationstation.bits.value1 < io.reservationstation.bits.value2),
       // Greater Than (signed)
       (instructionChecker.output.branch === BranchOperations.GreaterOrEqual)
-        -> Mux(io.reservationstation.bits.value1.asSInt >= io.reservationstation.bits.value2.asSInt,
-        newProgramCounter, 0.S),
+        -> (io.reservationstation.bits.value1.asSInt >= io.reservationstation.bits.value2.asSInt),
       // Greater Than (unsigned)
       (instructionChecker.output.branch === BranchOperations.GreaterOrEqualUnsigned)
-        -> Mux(io.reservationstation.bits.value1 >= io.reservationstation.bits.value2,
-        newProgramCounter, 0.S),
-      // jal or auipc
-      (instructionChecker.output.instruction === Instructions.auipc || instructionChecker.output.instruction === Instructions.jal)
-        -> (io.reservationstation.bits.programCounter + io.reservationstation.bits.value2.asSInt),
-      // jalr
-      (instructionChecker.output.instruction === Instructions.jalr)
-        -> Cat((io.reservationstation.bits.value1 + io.reservationstation.bits.value2)(63, 1), 0.U).asSInt
+        -> (io.reservationstation.bits.value1 >= io.reservationstation.bits.value2)
     ))
+
     io.fetch.valid := (instructionChecker.output.instruction === Instructions.Branch) || (instructionChecker.output.instruction === Instructions.jal) ||
       (instructionChecker.output.instruction === Instructions.auipc) || (instructionChecker.output.instruction === Instructions.jalr)
+    when(io.fetch.valid) {
+      io.fetch.programCounter := MuxCase(io.reservationstation.bits.programCounter, Seq(
+        // 分岐
+        // Equal
+        (instructionChecker.output.branch === BranchOperations.Equal)
+          -> Mux(io.reservationstation.bits.value1 === io.reservationstation.bits.value2,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // NotEqual
+        (instructionChecker.output.branch === BranchOperations.NotEqual)
+          -> Mux(io.reservationstation.bits.value1 =/= io.reservationstation.bits.value2,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // Less Than (signed)
+        (instructionChecker.output.branch === BranchOperations.LessThan)
+          -> Mux(io.reservationstation.bits.value1.asSInt < io.reservationstation.bits.value2.asSInt,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // Less Than (unsigned)
+        (instructionChecker.output.branch === BranchOperations.LessThanUnsigned)
+          -> Mux(io.reservationstation.bits.value1 < io.reservationstation.bits.value2,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // Greater Than (signed)
+        (instructionChecker.output.branch === BranchOperations.GreaterOrEqual)
+          -> Mux(io.reservationstation.bits.value1.asSInt >= io.reservationstation.bits.value2.asSInt,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // Greater Than (unsigned)
+        (instructionChecker.output.branch === BranchOperations.GreaterOrEqualUnsigned)
+          -> Mux(io.reservationstation.bits.value1 >= io.reservationstation.bits.value2,
+          branchedProgramCounter, io.reservationstation.bits.programCounter + 4.S),
+        // jal or auipc
+        (instructionChecker.output.instruction === Instructions.auipc || instructionChecker.output.instruction === Instructions.jal)
+          -> (io.reservationstation.bits.programCounter + io.reservationstation.bits.value2.asSInt),
+        // jalr
+        (instructionChecker.output.instruction === Instructions.jalr)
+          -> Cat((io.reservationstation.bits.value1 + io.reservationstation.bits.value2) (63, 1), 0.U).asSInt
+      ))
+    }.otherwise {
+      io.fetch.programCounter := io.reservationstation.bits.programCounter + 4.S
+    }
     // S形式(LSQへアドレスを渡す)
     io.loadstorequeue.programCounter := io.reservationstation.bits.programCounter
     io.loadstorequeue.destinationTag := io.reservationstation.bits.destinationTag
