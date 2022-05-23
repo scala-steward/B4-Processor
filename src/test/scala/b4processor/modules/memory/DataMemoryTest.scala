5411d009dd488befa898369439ef851397a77cc3
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-05-23 09:23:16 +0900

    データメモリのテスト

---
+++ src/test/scala/b4processor/modules/memory/DataMemoryTest.scala
@@ -0,0 +1,10 @@
+package b4processor.modules.memory
+
+import b4processor.Parameters
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class DataMemoryTest extends AnyFlatSpec with ChiselScalatestTester {
+
+}
