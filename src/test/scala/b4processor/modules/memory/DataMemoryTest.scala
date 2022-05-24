0ebb2fafa146fb89f4a53ee5a2854883b0129680
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-05-24 14:53:10 +0900

    データメモリのテスト

--- src/test/scala/b4processor/modules/memory/DataMemoryTest.scala
+++ src/test/scala/b4processor/modules/memory/DataMemoryTest.scala
@@ -1,10 +1,11 @@
 package b4processor.modules.memory
 
 import b4processor.Parameters
+import b4processor.utils.DataMemoryValue
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class DataMemoryTest extends AnyFlatSpec with ChiselScalatestTester {
-
+class DataMemoryTestWrapper(implicit params: Parameters) extends AnyFlatSpec with ChiselScalatestTester {
+  def setMemory(values: Seq[Option[DataMemoryValue]] = Seq.fill(params.))
 }
---
+++ src/test/scala/b4processor/utils/DataMemoryValue.scala
@@ -0,0 +1,8 @@
+package b4processor.utils
+
+case class DataMemoryValue(address: Int,
+                           tag: Int,
+                           data: Int,
+                           opcode: Int,
+                           function3: Int
+                          )
