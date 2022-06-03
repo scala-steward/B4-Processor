34d44b634e7c4e608be7c5945ce2ab3a50f1e8d7
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-06-03 19:44:11 +0900

    DataMemoryBuffer

---
+++ src/main/scala/b4processor/modules/memory/DataMemoryBuffer.scala
@@ -0,0 +1,25 @@
+package b4processor.modules.memory
+
+import b4processor.Parameters
+import b4processor.connections
+import b4processor.connections.LoadStoreQueue2Memory
+import chisel3._
+import chisel3.stage.ChiselStage
+import chisel3.util._
+
+/**
+ * from LSQ toDataMemory のためのバッファ
+ *
+ * @param params パラメータ
+ */
+class DataMemoryBuffer(implicit  params: Parameters) extends Module {
+  val io = IO(new Bundle {
+    val dataIn = Vec(params.maxLSQ2MemoryinstCount, Flipped(new LoadStoreQueue2Memory))
+    val dataOut = new LoadStoreQueue2Memory
+    val head =
+  })
+
+  // enqueue
+
+  // dequeue
+}
