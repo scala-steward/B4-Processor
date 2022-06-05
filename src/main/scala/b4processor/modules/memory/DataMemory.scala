b98c1eb57e5f91ca7180bb6092670b037cd8fd22
Michiya Kato <s194264q@st.go.tuat.ac.jp>
2022-06-06 03:18:07 +0900

    DataMemory

--- src/main/scala/b4processor/modules/memory/DataMemory.scala
+++ src/main/scala/b4processor/modules/memory/DataMemory.scala
@@ -13,37 +13,44 @@ class DataMemory(implicit params: Parameters) extends Module {
   })
 
   val mem = SyncReadMem(math.pow(2, params.tagWidth).toInt, UInt(64.W))
-
   io.dataOut := DontCare
 
   io.dataIn.ready := true.B
-
   when(io.dataIn.valid) {
     val rdwrPort = mem(io.dataIn.bits.address.asUInt)
-    when(io.dataIn.bits.opcode === "b0000011".U) {
-      // Load
+    when(io.dataIn.bits.opcode === "b0100011".U) {
+      // printf(p"dataIn =${io.dataIn.bits.data}\n")
+      // Store
+      /** writeの場合，rdwrPortは命令実行時の次クロック立ち上がりでmemoryに書き込み(=ストア命令実行時では値変わらず)*/
       rdwrPort := MuxLookup(io.dataIn.bits.function3, 0.U, Seq(
-        "b000".U -> Mux(io.dataIn.bits.data(7), Cat(0.U(56.W), io.dataIn.bits.data(7, 0)), Cat(1.U(56.W), io.dataIn.bits.data(7, 0))),
-        "b001".U -> Mux(io.dataIn.bits.data(15), Cat(0.U(48.W), io.dataIn.bits.data(15, 0)), Cat(1.U(48.W), io.dataIn.bits.data(15, 0))),
-        "b010".U -> Mux(io.dataIn.bits.data(31), Cat(0.U(32.W), io.dataIn.bits.data(31, 0)), Cat(1.U(32.W), io.dataIn.bits.data(31, 0))),
-        "b011".U -> io.dataIn.bits.data,
-        "b100".U -> Cat(0.U(56.W), io.dataIn.bits.data(7, 0)),
-        "b101".U -> Cat(0.U(48.W), io.dataIn.bits.data(15, 0)),
-        "b110".U -> Cat(0.U(32.W), io.dataIn.bits.data(31, 0))
+        "b000".U -> Mux(io.dataIn.bits.data(7), Cat(~0.U(56.W), io.dataIn.bits.data(7, 0)), Cat(0.U(56.W), io.dataIn.bits.data(7, 0))),
+        "b001".U -> Mux(io.dataIn.bits.data(15), Cat(~0.U(48.W), io.dataIn.bits.data(15, 0)), Cat(0.U(48.W), io.dataIn.bits.data(15, 0))),
+        "b010".U -> Mux(io.dataIn.bits.data(31), Cat(~0.U(32.W), io.dataIn.bits.data(31, 0)), Cat(0.U(32.W), io.dataIn.bits.data(31, 0))),
+        "b011".U -> io.dataIn.bits.data
       ))
     }.otherwise {
-      // Store
+      // Load
+      /** readの場合，rdwrPortは命令実行時と同クロック立ち上がりでmemoryから読み込み(=ロード命令実行時に値変更)*/
       io.dataOut.bits.data := MuxLookup(io.dataIn.bits.function3, 0.U, Seq(
-        "b000".U -> Mux(rdwrPort(7), Cat(0.U(56.W), rdwrPort(7, 0)), Cat(1.U(56.W), rdwrPort(7, 0))),
-        "b001".U -> Mux(rdwrPort(15), Cat(0.U(48.W), rdwrPort(15, 0)), Cat(1.U(48.W), rdwrPort(15, 0))),
-        "b010".U -> Mux(rdwrPort(31), Cat(0.U(32.W), rdwrPort(31, 0)), Cat(1.U(32.W), rdwrPort(31, 0))),
+        "b000".U -> Mux(rdwrPort(7), Cat(~0.U(56.W), rdwrPort(7, 0)), Cat(0.U(56.W), rdwrPort(7, 0))),
+        "b001".U -> Mux(rdwrPort(15), Cat(~0.U(48.W), rdwrPort(15, 0)), Cat(0.U(48.W), rdwrPort(15, 0))),
+        "b010".U -> Mux(rdwrPort(31), Cat(~0.U(32.W), rdwrPort(31, 0)), Cat(0.U(32.W), rdwrPort(31, 0))),
         "b011".U -> rdwrPort,
+        "b100".U -> Cat(0.U(56.W), rdwrPort(7, 0)),
+        "b101".U -> Cat(0.U(48.W), rdwrPort(15, 0)),
+        "b110".U -> Cat(0.U(32.W), rdwrPort(31, 0))
       ))
+      // printf(p"rdwrPort(7) = ${rdwrPort(7)}\n")
+      // printf(p"rdwrPort(7, 0) = ${rdwrPort(7, 0)}\n")
+      // printf(p"dataOut = ${io.dataOut.bits.data}\n")
     }
+    // printf(p"rdwrPort =${rdwrPort}\n")
     io.dataOut.bits.tag := io.dataIn.bits.tag
   }
-  io.dataOut.valid := io.dataIn.valid
+  io.dataOut.valid := io.dataIn.bits.opcode === "b0000011".U
+  // printf(p"mem(io.dataIn.bits.address.asUInt) = ${mem(io.dataIn.bits.address.asUInt)}\n")
 }
+
 
 object DataMemoryElaborate extends App {
   implicit val params = Parameters(numberOfDecoders = 1, numberOfALUs = 1, maxRegisterFileCommitCount = 1, tagWidth = 4)
--- src/test/scala/b4processor/modules/memory/DataMemoryTest.scala
+++ src/test/scala/b4processor/modules/memory/DataMemoryTest.scala
@@ -15,22 +15,56 @@ class DataMemoryTest extends AnyFlatSpec with ChiselScalatestTester {
 
   it should "store and load a value" in {
     test(new DataMemoryTestWrapper) { c =>
+      // 0アドレスへのストア
       c.io.dataIn.bits.address.poke(0)
       c.io.dataIn.bits.data.poke(123)
-      c.io.dataIn.bits.tag.poke(0)
+      c.io.dataIn.bits.tag.poke(10)
       c.io.dataIn.bits.function3.poke("b011".U)
-      c.io.dataIn.bits.opcode.poke("b0000011".U)
+      c.io.dataIn.bits.opcode.poke("b0100011".U)
       c.io.dataIn.valid.poke(true)
 
-      c.clock.step()
-
-      c.io.dataIn.bits.address.poke(0)
-      c.io.dataIn.bits.data.poke(123)
-      c.io.dataIn.bits.tag.poke(0)
+      c.clock.step(1)
+      // 別アドレスへのストア
+      c.io.dataIn.bits.address.poke(40)
+      c.io.dataIn.bits.data.poke(1000)
+      c.io.dataIn.bits.tag.poke(30)
       c.io.dataIn.bits.function3.poke("b011".U)
-      c.io.dataIn.bits.opcode.poke("b0000010".U)
+      c.io.dataIn.bits.opcode.poke("b0100011".U)
+      c.io.dataIn.valid.poke(true)
+
+      c.clock.step(2)
+      // 0アドレスからのロード
+      c.io.dataIn.bits.address.poke(0)
+      c.io.dataIn.bits.data.poke(0)
+      c.io.dataIn.bits.tag.poke(20)
+      c.io.dataIn.bits.function3.poke("b011".U)
+      c.io.dataIn.bits.opcode.poke("b0000011".U)
       c.io.dataOut.valid.expect(true)
       c.io.dataOut.bits.data.expect(123)
+      c.io.dataOut.bits.tag.expect(20)
+    }
+  }
+
+  it should "load a byte value" in {
+    test(new DataMemoryTestWrapper) { c =>
+      // 0アドレスへのストア
+      c.io.dataIn.bits.address.poke(0)
+      c.io.dataIn.bits.data.poke("b10000000011".U)
+      c.io.dataIn.bits.tag.poke(10)
+      c.io.dataIn.bits.function3.poke("b011".U)
+      c.io.dataIn.bits.opcode.poke("b0100011".U)
+      c.io.dataIn.valid.poke(true)
+
+      c.clock.step(2)
+      // 0アドレスからのロード
+      c.io.dataIn.bits.address.poke(0)
+      c.io.dataIn.bits.data.poke(0)
+      c.io.dataIn.bits.tag.poke(20)
+      c.io.dataIn.bits.function3.poke("b000".U)
+      c.io.dataIn.bits.opcode.poke("b0000011".U)
+      c.io.dataOut.valid.expect(true)
+      c.io.dataOut.bits.data.expect(3)
+      c.io.dataOut.bits.tag.expect(20)
     }
   }
 }
